4521c31d04372c5b28faf4e61a360fa2f0781c56
jamesii-b <jamesbhattarai14@gmail.com>
2023-12-31 11:16:08 +0545

    Feat: data display in dashboard

---
+++ lib/models/deliveryData.dart
@@ -0,0 +1,74 @@
+class DeliveryData {
+  String date;
+  String delivered;
+  String id;
+  String latitudeFrom;
+  String latitudeTo;
+  String longitudeFrom;
+  String longitudeTo;
+  String productID;
+  String username;
+  String vehicleNumber;
+
+  DeliveryData({
+    required this.date,
+    required this.delivered,
+    required this.id,
+    required this.latitudeFrom,
+    required this.latitudeTo,
+    required this.longitudeFrom,
+    required this.longitudeTo,
+    required this.productID,
+    required this.username,
+    required this.vehicleNumber,
+  });
+
+  factory DeliveryData.fromJson(Map<String, dynamic> json) {
+    return DeliveryData(
+      date: json['date'],
+      delivered: json['delivered'],
+      id: json['id'],
+      latitudeFrom: json['latitudeFrom'],
+      latitudeTo: json['latitudeTo'],
+      longitudeFrom: json['longitudeFrom'],
+      longitudeTo: json['longitudeTo'],
+      productID: json['productID'],
+      username: json['username'],
+      vehicleNumber: json['vehicleNumber'],
+    );
+  }
+
+  String getDate() => date;
+
+  String getDelivered() => delivered;
+
+  String getId() => id;
+
+  String getLatitudeFrom() => latitudeFrom;
+
+  String getLatitudeTo() => latitudeTo;
+
+  String getLongitudeFrom() => longitudeFrom;
+
+  String getLongitudeTo() => longitudeTo;
+
+  String getProductID() => productID;
+
+  String getUsername() => username;
+
+  String getVehicleNumber() => vehicleNumber;
+  @override
+  String toString() {
+    return 'DeliveryData {'
+        'date: $date, '
+        'delivered: $delivered, '
+        'id: $id, '
+        'latitudeFrom: $latitudeFrom, '
+        'latitudeTo: $latitudeTo, '
+        'longitudeFrom: $longitudeFrom, '
+        'longitudeTo: $longitudeTo, '
+        'productID: $productID, '
+        'username: $username, '
+        'vehicleNumber: $vehicleNumber}';
+  }
+}
--- lib/views/pages/dashboard/dashboard.dart
+++ lib/views/pages/dashboard/dashboard.dart
@@ -1,42 +1,152 @@
 import 'dart:async';
+import 'dart:convert';
+
+import 'package:bazarmilo/models/deliveryData.dart';
 import 'package:bazarmilo/provider/loginstate.dart';
 import 'package:bazarmilo/views/components/navbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 class Dashboard extends StatefulWidget {
-  const Dashboard({super.key});
+  const Dashboard({Key? key}) : super(key: key);
 
   @override
   State<Dashboard> createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
-  bool isOnline = false;
-
-  String firstName = "John";
+  late String username = "";
+  String httpUrl = dotenv.env['API_URL'] ?? "";
+  String completeUrl = "";
+  late List<DeliveryData> deliveryData = [];
 
   @override
   void initState() {
+    super.initState();
     WidgetsBinding.instance!.addPostFrameCallback((_) {
       checkLoginStatus(context);
     });
-    super.initState();
+    completeUrl = "$httpUrl/tasks";
+    username = Provider.of<LoginProvider>(context, listen: false).username;
+    // Use the then method to handle the asynchronous work
+    getDeliveryData(completeUrl, username).then((_) {
+      // This will be called after the asynchronous call is complete
+      // Set state here if needed
+      setState(() {});
+    });
   }
 
   Future<void> checkLoginStatus(BuildContext context) async {
-    var loginProvider = Provider.of<LoginProvider>(context, listen: false);
+    var provider = Provider.of<LoginProvider>(context, listen: false);
 
-    if (!loginProvider.isLogin) {
+    if (!provider.isLogin) {
       Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
+
+  Future<void> getDeliveryData(String url, String username) async {
+    try {
+      var response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        String localDate = DateTime.now().toString().substring(0, 10);
+        print(localDate);
+        List<dynamic> data = json.decode(response.body);
+        List<DeliveryData> filteredData = data
+            .where((item) =>
+                item['username'] == username &&
+                item['date'] ==
+                    localDate) // You can customize the date comparison
+            .map((item) => DeliveryData.fromJson(item))
+            .toList();
+        // Set the state after the data is filtered
+        setState(() {
+        deliveryData = filteredData;
+        });
+    // Print each instance's details
+      deliveryData.forEach((data) {
+        print(data);
+      });
+      } else {
+        throw Exception('Failed to load travel data');
+      }
+    } catch (e) {
+      print('Error fetching travel data: $e');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: bottomNavigationBar(currentPageIndex: 0),
-        body: Text("dashboard"));
+      bottomNavigationBar: bottomNavigationBar(currentPageIndex: 0),
+      body: Column(
+        children: [
+          Text("love is a waste of time?"),
+          DisplayInfo(
+            deliveryData: deliveryData,
+          ),
+        ],
+      ),
+    );
   }
 }
 
+class DisplayInfo extends StatefulWidget {
+  late List<DeliveryData> deliveryData = [];
+
+  DisplayInfo({super.key, required this.deliveryData});
+
+  @override
+  State<DisplayInfo> createState() => _DisplayInfoState();
+}
+
+class _DisplayInfoState extends State<DisplayInfo> {
+  @override
+  Widget build(BuildContext context) {
+    final deliveryData = widget.deliveryData;
+
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: Column(
+            children: (deliveryData != null && deliveryData.isNotEmpty)
+                ? deliveryData.map((data) {
+                    print(data.getDate());
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        child: ListTile(
+                          title: Text("Date: ${data.getDate()}"),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Delivered: ${data.getDelivered()}"),
+                              Text("ID: ${data.getId()}"),
+                              Text(
+                                  "From: (${data.getLatitudeFrom()}, ${data.getLongitudeFrom()})"),
+                              Text(
+                                  "To: (${data.getLatitudeTo()}, ${data.getLongitudeTo()})"),
+                              Text("Product ID: ${data.getProductID()}"),
+                              Text("Username: ${data.getUsername()}"),
+                              Text(
+                                  "Vehicle Number: ${data.getVehicleNumber()}"),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList()
+                : [
+                    // Display a message when there is no data
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("No delivery data available."),
+                    ),
+                  ],
+          ),
+        ),
+      ],
+    );
+  }
+}
